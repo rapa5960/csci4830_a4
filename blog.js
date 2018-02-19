21c59625547163ce0554c7c27f795f9c8389b3f7
rapa5960 <rapa5960@colorado.edu>
2018-02-19 10:35:56 -0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#element").show();
+	$("#otherElement").hide();
+});
+
+$("#buttoncomment").click(function() { // when "button_id" is clicked
+	$("#alert").show(); // show element
+});
